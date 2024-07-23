559f7e3499903c2159567202b53f06fe1cf376ca
HongJo9 <luis.chavez.kook@gmail.com>
2024-07-23 16:03:08 -0500

    este commit es donde mofifique algo de diseño, dando una interfaz de filtro en tienda, yen home dandonle portadas

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,6 +13,21 @@ export default function Home({
   return (
     <>
       <div className="home">
+        <div>
+          <img className="rounded-3xl" src="portada general.jpg" alt="" />
+        </div>
+        <div>
+        <div className="flex">
+            <div><img className="rounded-3xl " src="portada general.jpg" alt="" /></div>
+            <div className="w-1/4"><img className="rounded-3xl" src="portada general.jpg" alt="" /></div>
+            <div className="w-1/2"><img className="rounded-3xl" src="portada general.jpg" alt="" /></div>
+          </div>
+          <div className="flex">
+            <div className="w-1/2"><img className="rounded-3xl" src="portada general.jpg" alt="" /></div>
+            <div className="w-1/4"><img className="rounded-3xl" src="portada general.jpg" alt="" /></div>
+            <div className="w-1/4"><img className="rounded-3xl" src="portada general.jpg" alt="" /></div>
+          </div>
+        </div>
         <ListaProductos
           carrito={carrito}
           setCarrito={setCarrito}
