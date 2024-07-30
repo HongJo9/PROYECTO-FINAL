6b05b73727285619867fd2c47aa51526026c4310
HongJo9 <luis.chavez.kook@gmail.com>
2024-07-29 22:40:55 -0500

    Correcion de las imagenes no reconocidas

--- src/components/PortadasHome.jsx
+++ src/components/PortadasHome.jsx
@@ -10,16 +10,17 @@ export default function PortadasHome() {
       <div className="portada-home1 p-5">
         <div className="media-portada">
           <div className="portada-home2 pb-5">
-            <div className="portada-home211 rounded-3xl"></div>
-            <div className="portada-home212 rounded-3xl"></div>
+            <div className="portada-home211 rounded-3xl" style={{ backgroundImage: "url('Portada 2-11.jpg')" }}></div>
+            <div className="portada-home212 rounded-3xl" style={{ backgroundImage: "url('Portada 2-12.jpg')" }}></div>
           </div>
-          <div className="portada-home21 rounded-3xl"></div>
+          <div className="portada-home21 rounded-3xl" style={{ backgroundImage: "url('Portada-2-1.jpg')" }}></div>
         </div>
+
         <div>
-          <div className="portada-home22 rounded-3xl"></div>
+          <div className="portada-home22 rounded-3xl" style={{ backgroundImage: "url('Portada 2-2.jpg')" }}></div>
           <div className="portada-home2 py-5">
-            <div className="portada-home221 rounded-3xl"></div>
-            <div className="portada-home222 rounded-3xl"></div>
+            <div className="portada-home221 rounded-3xl" style={{ backgroundImage: "url('Portada 2-21.jpg')" }}></div>
+            <div className="portada-home222 rounded-3xl" style={{ backgroundImage: "url('Portada 2-22.jpg')" }}></div>
           </div>
         </div>
       </div>
